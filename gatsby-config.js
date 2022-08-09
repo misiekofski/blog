6590be32538e52a6913381d5708ce98164cbcd05
Michal Dobrzycki <dobrzycki@gmail.com>
2022-08-09 19:17:48 +0200

    Removed old posts, added first one

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `SCV Consultants Blog`,
+    siteTitleAlt: `SCV Consultants Blog - Quality Assurance, DevFX and Software Craftmanship`,
+    siteHeadline: `SCV Consultants Blog - Quality Assurance, DevFX and Software Craftmanship`,
+    siteUrl: `https://scvconsultants.gtsb.io/`,
+    siteDescription: `SCV Consultants Blog - Quality Assurance, DevFX and Software Craftmanship`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@MDobrzycki`,
   },
   plugins: [
     {
@@ -34,11 +34,11 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/MDobrzycki`,
           },
           {
             name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            url: `https://https://scvconsultants.gtsb.io`,
           },
         ],
       },
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,7 @@ import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 const NotFound = () => (
   <Layout>
     <h1>404 - Page Not Found</h1>
-    <p>Unfortunately we couldn't find what you were looking for :(</p>
+    <p>OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!</p>
   </Layout>
 )
 
