aa6a928ae28b9459d72f2d230752c43be3f6e965
Michal Dobrzycki <dobrzycki@gmail.com>
2022-10-09 14:27:48 +0200

    Fixed gatbsy-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -153,7 +153,7 @@ module.exports = {
       },
       resolve: 'gatsby-plugin-mailchimp',
       options: {
-          endpoint: 'process.env.MAILCHIMP_ENDPOINT',
+          endpoint: process.env.MAILCHIMP_ENDPOINT,
           timeout: 3500,
       },
     },
