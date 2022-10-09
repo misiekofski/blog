0617aae85a3c40afcb380bac69954ebc13b0e0fe
Michal Dobrzycki <dobrzycki@gmail.com>
2022-10-09 18:18:22 +0200

    Changed text for subscription component

--- src/@lekoarts/gatsby-theme-minimal-blog/texts/Subscribe.jsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/texts/Subscribe.jsx
@@ -33,7 +33,9 @@ export default class Subscribe extends React.Component {
       <React.Fragment>
         <div className="subscribe">
           <h3 className="subscribeInfo">
-            Like the article? Subscribe to get notified whenever a new article gets published!
+            I promise: no ads, no commercial content, your data stays solely with me.
+            <br />
+            Quality content (in text format) directly to your mail inbox.
           </h3>
           <Form name="subscribe" onFinish={this.handleSubmit} layout="horizontal">
             <Form.Item
