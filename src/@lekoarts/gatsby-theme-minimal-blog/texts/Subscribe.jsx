7da03bccf3c100e92c9db8703ed9ed54a1904edb
Michal Dobrzycki <dobrzycki@gmail.com>
2022-10-09 18:13:24 +0200

    Fixed styling for subscribe component

--- src/@lekoarts/gatsby-theme-minimal-blog/texts/Subscribe.jsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/texts/Subscribe.jsx
@@ -31,12 +31,11 @@ export default class Subscribe extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Divider />
         <div className="subscribe">
           <h3 className="subscribeInfo">
             Like the article? Subscribe to get notified whenever a new article gets published!
           </h3>
-          <Form name="subscribe" onFinish={this.handleSubmit} layout="inline">
+          <Form name="subscribe" onFinish={this.handleSubmit} layout="horizontal">
             <Form.Item
               label="E-mail"
               name="email"
@@ -53,25 +52,12 @@ export default class Subscribe extends React.Component {
               <Input prefix={<UserOutlined />} onChange={this.handleNameChange} />
             </Form.Item>
             <Form.Item>
-              <Button type="primary" htmlType="submit" shape="round">
+              <Button block type="primary" htmlType="submit" size='large'>
                 Subscribe
               </Button>
             </Form.Item>
           </Form>
         </div>
-
-        {/* --- STYLES --- */}
-        <style jsx>{`
-          .subscribe {
-            .subscribeInfo {
-              text-align: center;
-              padding-bottom: 20px;
-            }
-          }
-          .ant-form .ant-form-inline {
-            justify-content: center;
-          }
-        `}</style>
       </React.Fragment>
     )
   }
